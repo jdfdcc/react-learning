5e2a152eef004eddae8c07c7d7ea631dcbf67507
lla <myllauser@gmail.com>
2017-05-02 11:09:46 +0800

    test master

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ render(<div>
 	     <Note content="测试便签二号" date="2017-5-1"/>
 	     <Note content="测试便签三号" date="2017-5-1"/>
 	     <Note content="测试便签四号" date="2017-5-1"/>
+	     <Note content="测试master" date="2017-5-1"/>
        </div>,document.getElementById('app'))
